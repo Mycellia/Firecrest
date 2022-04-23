665ed10fb039d0d2df60cdca5e1cae275671c4b4
nfo00251 <naiemaolagunju@gmail.com>
2022-04-23 19:32:11 +0100

    created header
    created and styled the header

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Container,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <Navbar className="color-nav" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/assets/Firecrest-logo.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          Firecrest
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className="link" as={Link} to="/">
+              Book
+            </Nav.Link>
+            <Nav.Link className="link" as={Link} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link className="link" as={Link} to="/about">
+              Contact Us
+            </Nav.Link>
+          </Nav>
+          <Button className="button" variant="outline-success">
+            Sign In
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+export default Header;
