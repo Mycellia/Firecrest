65f2226a74960ab725be5bfc65515d682368d7a5
nfo00251 <naiemaolagunju@gmail.com>
2022-04-26 00:51:37 +0100

    hero section
    created a hero section for the homepage

---
+++ frontend/src/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeroImage, HeroSection } from "./HeroStyles";
+
+const Hero = () => {
+  return (
+    <HeroSection>
+      <HeroImage src="./assets/Firecrest.png" />
+    </HeroSection>
+  );
+};
+
+export default Hero;
