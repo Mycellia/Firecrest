48ad93ba0216e72ab4c0898871e5781ddc9160a1
nfo00251 <naiemaolagunju@gmail.com>
2022-04-27 14:33:59 +0100

    updated the footer
    updated the styling and content of the footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,36 +1,41 @@
 import React from "react";
-import {
-  Box,
-  Container,
-  Row,
-  Column,
-  FooterLink,
-  Heading,
-  FooterRights,
-} from "./FooterStyle";
+import "./Footer.css";
+import { Link } from "react-router-dom";
+import { Button } from "../../globalStyles";
 
-const Footer = () => {
+function Footer() {
   return (
-    <Box>
-      <Container>
-        <Row>
-          <Column>
-            <FooterLink href="#">Home</FooterLink>
-            <FooterLink href="#">About</FooterLink>
-            <FooterLink href="#">Book</FooterLink>
-          </Column>
-          <Column>
-            <FooterLink href="#">Contact Us</FooterLink>
-            <FooterLink href="#">How We Operate</FooterLink>
-          </Column>
-          <Column>
-            <FooterLink href="#">Contact Us</FooterLink>
-            <FooterLink href="#">How We Operate</FooterLink>
-          </Column>
-        </Row>
-      </Container>
-      <FooterRights>Firecrest © 2022</FooterRights>
-    </Box>
+    <div className="footer-container">
+      <div class="footer-links">
+        <div className="footer-link-wrapper">
+          <div class="footer-link-items">
+            <Link to="/signup">How it works</Link>
+            <Link to="/about">About</Link>
+            <Link to="/book">Book</Link>
+            <Link to="/contact-us">Contact Us</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div class="footer-link-items-subscription">
+            <p className="footer-subscription-text">
+              You can unsubscribe at any time.
+            </p>
+            <div className="input-areas">
+              <form className="input-areas">
+                <input
+                  className="footer-input"
+                  name="email"
+                  type="email"
+                  placeholder="Your Email"
+                />
+              </form>
+              <Button buttonStyle="btn--outline">Subscribe</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-};
+}
+
 export default Footer;
