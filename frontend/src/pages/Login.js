5117779a515d7097ff2382f18edbe8a850520dc2
nfo00251 <naiemaolagunju@gmail.com>
2022-04-27 16:08:42 +0100

    Sign in page
    created the sign in page

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+import { login } from "../actions/auth";
+import { FormButton } from "./Contact/ContactStyles";
+import { Container } from "../globalStyles";
+
+export const Login = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+  if (isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Container>
+      <div className="container mt-5">
+        <h1>Sign In</h1>
+        <p>Sign into your Account</p>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => onChange(e)}
+              minLength="6"
+              required
+            />
+          </div>
+          <FormButton type="submit">Sign In</FormButton>
+        </form>
+
+        <p className="mt-3">
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </p>
+        <p className="mt-3">
+          Forgot your Password? <Link to="/reset-password">Reset Password</Link>
+        </p>
+      </div>
+    </Container>
+  );
+};
+
+// const mapStateToProps = state => ({
+
+// });
+
+export default connect(null, { login })(Login);
