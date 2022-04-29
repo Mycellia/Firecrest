26b32c458101fe2e5ad8707f954cfdb569928d2b
nfo00251 <naiemaolagunju@gmail.com>
2022-04-29 08:16:19 +0100

    app.css deleted
    deleted app.css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
