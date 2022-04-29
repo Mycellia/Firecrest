5f7b57f56612efa2206ad29023ca80b6ab9e8f41
nfo00251 <naiemaolagunju@gmail.com>
2022-04-29 08:14:39 +0100

    app,js
    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,37 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { About } from "./pages/About";
 import Footer from "./components/Footer/Footer";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact/Contact";
+import Activate from "./pages/Activate";
+import ResetPassword from "./pages/ResetPassword";
+import ResetPasswordConfirm from "./pages/ResetPasswordConfirm";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <BrowserRouter>
         <Header />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/activate/:uid/:token" element={<Activate />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path=" :uid/:token" element={<ResetPasswordConfirm />} />
         </Routes>
         <Footer />
       </BrowserRouter>
-    </>
+    </Provider>
   );
 }
 
