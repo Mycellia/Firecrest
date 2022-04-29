edc4c9247289c849cd222f21d6505eec9bb419b7
nfo00251 <naiemaolagunju@gmail.com>
2022-04-29 08:13:42 +0100

    password reset
    resetting your password and authentication

---
+++ frontend/src/pages/ResetPassword.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+import { reset_password } from "../actions/auth";
+import { Container } from "../Styles";
+import { FormButton } from "./Contact/ContactStyles";
+
+const ResetPassword = ({ reset_password }) => {
+  const [requestSent, setRequestSent] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+  });
+
+  const { email } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    reset_password(email);
+    setRequestSent(true);
+  };
+
+  if (requestSent) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Container>
+      <div className="container mt-5">
+        <h1>Password Reset Request:</h1>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <FormButton type="submit">Reset</FormButton>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default connect(null, { reset_password })(ResetPassword);
---
+++ frontend/src/pages/ResetPasswordConfirm.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+import { reset_password_confirm } from "../actions/auth";
+import { Button, Container } from "../Styles";
+import { FormButton } from "./Contact/ContactStyles";
+
+const ResetPasswordConfirm = ({ match, reset_password_confirm }) => {
+  const [requestSent, setRequestSent] = useState(false);
+  const [formData, setFormData] = useState({
+    new_password: "",
+    re_new_password: "",
+  });
+
+  const { new_password, re_new_password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const uid = match.params.uid;
+    const token = match.params.token;
+
+    reset_password_confirm(uid, token, new_password, re_new_password);
+    setRequestSent(true);
+  };
+
+  if (requestSent) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Container>
+      <div className="container mt-5">
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="password"
+              placeholder="Type New Password"
+              name="new_password"
+              value={new_password}
+              onChange={(e) => onChange(e)}
+              minLength="6"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="password"
+              placeholder="Re-type New Password"
+              name="re_new_password"
+              value={re_new_password}
+              onChange={(e) => onChange(e)}
+              minLength="6"
+              required
+            />
+          </div>
+          <FormButton type="submit">Reset</FormButton>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default connect(null, { reset_password_confirm })(ResetPasswordConfirm);
